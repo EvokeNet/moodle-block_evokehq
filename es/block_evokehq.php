0ff97a83bb56ca926b74e73df542927f265be50f
John Moravec <51122406+educationfutures@users.noreply.github.com>
2021-10-18 09:48:12 -0500

    create new translation

---
+++ es/block_evokehq.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_evokehq
+ * @category    string
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Headquarters (Evoke Central)';
+$string['privacy:metadata'] = 'Evoke Central no almacena ningún dato personal';
+
+$string['page_groups_title'] = 'Grupos de la campaña';
+$string['myteam'] = 'Mi equipo';
+$string['exploreportfolios'] = 'Explore portafolios';
+$string['groupchat'] = 'Chat de equipo';
+$string['evokation'] = 'Evokación';  // RIGHT?
+$string['missions'] = 'Misiones';
+
+$string['networkcreations'] = 'Creaciones de red';
+
+$string['page_portfolios_title'] = 'Portafolios';
+$string['view'] = 'Ver';
+
+$string['page_section_title'] = 'Envíos del equipo';
+
+$string['configuration'] = 'Configuración del bloque Evoke Central';
+$string['url_chat'] = 'URL de chat grupal';
+$string['url_evokation'] = 'URL de evocación';
+
+$string['youarenotenrolled'] = '¡No está inscrito en un curso!';
+$string['youarenotenrolled_desc'] = 'Debe estar inscrito en un curso para ver la información del Evoke Central.';
+$string['therearenogroups'] = 'No hay equipos en este curso';
+$string['noportfolio'] = 'Esta campaña no tiene un portafolio';
+$string['nosubmissions'] = '¡No hay envíos!';
+$string['nosubmissions_desc'] = 'Aún no hay envíos de este equipo.';
