39a6fe498e62a793a1dd6932af62852e1d6a772b
Willian Mano <willianmanoaraujo@gmail.com>
2022-02-17 17:13:01 -0300

    Fixes string typo 2

--- classes/output/block.php
+++ classes/output/block.php
@@ -59,7 +59,7 @@ class block implements renderable, templatable {
         $data['url_chat'] = $this->config->chat ?? false;
         $data['url_evokation'] = $this->config->evokation ?? false;
 
-        $title = get_string('title', 'block_evokehq');
+        $title = get_string('pluginname', 'block_evokehq');
         $configtitle = isset($this->config->title) ? trim($this->config->title) : '';
         if (!empty($configtitle)) {
             $title = format_string($configtitle);
