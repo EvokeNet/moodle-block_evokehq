a90abe68baddca57fa3a8edfe56ca980ad66044f
Willian Mano <willian.mano@jove.com>
2023-03-31 11:58:38 -0300

    Add fr lang

---
+++ lang/fr/block_evokehq.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_evokehq
+ * @category    string
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Siège';
+$string['privacy:metadata'] = 'Le siège ne stocke aucune donnée personnelle';
+
+$string['page_groups_title'] = 'Groupes de campagne';
+$string['myteam'] = 'Mon groupe';
+$string['exploreportfolios'] = 'Explorer les portfolios';
+$string['exploreportfolios_desc'] = 'Vérifier la progression des autres';
+$string['groupchat'] = 'Discussion du groupe';
+$string['groupchat_desc'] = 'Parler à votre groupe';
+$string['evokation'] = 'Evokation: Projet final';
+$string['evokation_desc'] = 'Votre projet final';
+$string['missions'] = 'Les missions';
+$string['missions_desc'] = 'Vérifier vos devoirs personnels';
+$string['welcome'] = 'Bienvenue à';
+
+$string['networkcreations'] = 'Création de réseaux';
+
+$string['page_portfolios_title'] = 'Portfolios';
+$string['view'] = 'Visualiser';
+
+$string['page_section_title'] = 'Soumission du groupe';
+
+$string['configuration'] = ' Configuration des blocs du siège';
+$string['url_chat'] = 'URL de la discussion du groupe';
+$string['url_evokation'] = 'URL d\'Evokation';
+
+$string['youarenotenrolled'] = 'Vous n\'êtes pas inscrit à un cours!';
+$string['youarenotenrolled_desc'] = 'Vous devez être inscrit à un cours pour consulter les informations du siège.';
+$string['therearenogroups'] = 'Il n\'y a pas de groupe dans ce cours';
+$string['coursenoportfolio'] = 'Ce cours ne contient pas de portfolio';
+$string['nosubmissions'] = 'Pas de soumissions!';
+$string['nosubmissions_desc'] = 'Il n\'y a pas encore de soumissions de la part de ce groupe.';
+
+$string['page_portfolio_title'] = 'Soumissions de portfolios du groupe';
+$string['groupmembers'] = 'Members du groupe';
+$string['backtoportfolios'] = 'Retour aux portfolios';
+
+$string['config_title'] = 'Titre du bloc';
+$string['config_chat'] = 'URL de la discussion du groupe';
+$string['config_evokation'] = 'URL d\'Evokation';
+$string['config_portfolios'] = 'Explorer les portfolios URL';
+
+$string['page_missions_title'] = 'Carte de mission';
+$string['no_missions_map'] = 'Il n\'y a pas de carte de mission pour ce cours.';
+
+$string['block_links'] = 'Liens du bloc';
+$string['block_images'] = 'Images du bloc';
+$string['img_missions'] = 'Image de Missions';
+$string['img_evokation'] = 'Image d\'Evokation';
+$string['img_portfolios'] = 'Image de Portfolios';
+$string['img_chat'] = 'Image de discussion';
